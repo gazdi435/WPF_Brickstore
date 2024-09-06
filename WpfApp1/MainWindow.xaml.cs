eeea755ddc938741f9b8671a36042def6bc2a4de
gazdi435 <20.gazdag.zsolt@mechwart.com>
2024-09-06 10:20:35 +0200

    #3

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.AccessControl;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +9,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
+using System;
+using Microsoft.Win32;
 
 namespace WpfApp1
 {
@@ -16,9 +20,48 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static List<Part> PartList = new List<Part>();
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void betoltesBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string filename = "";
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.DefaultExt = ".bsx";
+            dlg.Filter = "BSX Files (*.bsx)|*.bsx";
+            if (dlg.ShowDialog() == true)
+            {
+                // Open document 
+                filename = dlg.FileName;
+            }
+
+            if (filename.Length > 0)
+            {
+                XDocument xaml = XDocument.Load(filename);
+                foreach (var elem in xaml.Descendants("Item"))
+                {
+                    PartList.Add(new Part(elem.Element("ItemID").Value, elem.Element("ItemName").Value, elem.Element("CategoryName").Value, elem.Element("ColorName").Value, Convert.ToInt32(elem.Element("Qty").Value)));
+                }
+
+                mainDG.ItemsSource = PartList;
+            }
+            else
+            {
+                MessageBox.Show("Hiba a fájl kiválasztása közben!");
+            }
+
+            
+            
+        }
+
+        private void itemIDTXTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            mainDG.ItemsSource = PartList.Where(x=> x.ItemID.StartsWith(itemIDTXTB.Text) && x.ItemName.StartsWith(itemNameTXTB.Text) && x.CategoryName.StartsWith(itemCategoryTXTB.Text));
         }
     }
 }
---
+++ WpfApp1/Part.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    internal class Part
+    {
+        string itemID;
+        string itemName;
+        string categoryName;
+        string colorName;
+        int qty;
+
+        public Part(string itemID, string itemName, string categoryName, string colorName, int qty)
+        {
+            this.itemID = itemID;
+            this.itemName = itemName;
+            this.categoryName = categoryName;
+            this.colorName = colorName;
+            this.qty = qty;
+        }
+
+        public string ItemID { get => itemID; }
+        public string ItemName { get => itemName; }
+        public string CategoryName { get => categoryName; }
+        public string ColorName { get => colorName; }
+        public int Qty { get => qty; }
+    }
+}
